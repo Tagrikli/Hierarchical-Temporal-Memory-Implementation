2e10b65124cbc3dcb7d4efd6b3299ea2e9200e3f
Tugrul Agrikli <tagrikli@gmail.com>
2021-12-04 01:54:02 +0300

    temporal

--- src/main.py
+++ src/main.py
@@ -9,9 +9,10 @@ input = np.zeros(input_size, dtype=np.int32)
 input[:10] = 1
 ##### SAMPLE INPUT ######
 
+
 fig, ax = plt.subplots(ncols=2)
 
 layer = OnlySpatial(input_size)
 layer.initSpatial()
 
-spatial_output = layer.spatialStep(input)
+a1 = layer.spatialStep(input)
--- src/temporal.py
+++ src/temporal.py
@@ -2,32 +2,46 @@ import numpy as np
 from config import *
 
 
-connections_x = np.random.randint(
+# Init temporal connections
+temporal_connections_columns = np.random.randint(
     0, COLUMN_COUNT, (COLUMN_COUNT, COLUMN_HEIGHT, TEMPORAL_CONNECTION_COUNT))
-connections_y = np.random.randint(
+temporal_connections_cells = np.random.randint(
     0, COLUMN_HEIGHT, (COLUMN_COUNT, COLUMN_HEIGHT, TEMPORAL_CONNECTION_COUNT))
-connections = np.stack((connections_x, connections_y), 3)
+temporal_connections = np.stack(
+    (temporal_connections_columns, temporal_connections_cells), 3)
 
-permen = np.random.random((COLUMN_COUNT, COLUMN_HEIGHT, TEMPORAL_CONNECTION_COUNT))
+# Init permenance values
+permenance_values = np.random.random(
+    (COLUMN_COUNT, COLUMN_HEIGHT, TEMPORAL_CONNECTION_COUNT))
 
+# Init cells status for active and predicting
 active_cells = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
-predic_cells = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
+predicting_cells = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
 
+cell_states = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
+##SAMPLE INPUT####################
 active_cells[:, :2] = 1
-predic_cells[0, 2] = 1
+predicting_cells[0, 2] = 1
 
-active_conns = active_cells[connections[:, :, :, 0], connections[:, :, :, 1]]
-predic_conns = predic_cells[connections[:, :, :, 0], connections[:, :, :, 1]]
+cell_states[:, :2] = 1
+cell_states[0, 2] = 2
+##SAMPLE INPUT####################
 
-connected_active_conns = np.ma.array(
-    active_conns, mask=(permen < PERMENANCE_THRESHOLD)).filled(fill_value=0)
 
-connected_predic_conns = np.ma.array(
-    predic_conns, mask=(permen < PERMENANCE_THRESHOLD)).filled(fill_value=0)
+active_connections = np.equal(cell_states[temporal_connections[:,
+                                                               :, :, 0], temporal_connections[:, :, :, 1]], CELL_STATE.ACTIVE)
+
+predicting_connections = np.equal(cell_states[temporal_connections[:,
+                                                                   :, :, 0], temporal_connections[:, :, :, 1]], CELL_STATE.PREDICTING)
+
+
+connected_active_conns = np.logical_and(
+    active_connections, permenance_values >= PERMENANCE_THRESHOLD
+)
+
+connected_predic_conns = np.logical_and(
+    predicting_connections, permenance_values >= PERMENANCE_THRESHOLD
+)
 
 connected_active_conns_overlap = np.sum(connected_active_conns, axis=2)
 connected_predic_conns_overlap = np.sum(connected_predic_conns, axis=2)
-
-print(connected_active_conns_overlap)
-print(connected_predic_conns_overlap)
-
