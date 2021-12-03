2e8d070b9a8ad152ce6c1e865608c197e02e8386
Tugrul Agrikli <tagrikli@gmail.com>
2021-12-04 02:17:50 +0300

    temporal

--- src/temporal.py
+++ src/temporal.py
@@ -20,12 +20,17 @@ predicting_cells = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
 
 cell_states = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
 ##SAMPLE INPUT####################
-active_cells[:, :2] = 1
-predicting_cells[0, 2] = 1
+column_input = np.zeros(COLUMN_COUNT)
+column_input[np.random.randint(0,COLUMN_COUNT,int(0.2 * COLUMN_COUNT))] = CELL_STATE.ACTIVE
+print(column_input)
 
-cell_states[:, :2] = 1
-cell_states[0, 2] = 2
+cell_states[:, :2] = CELL_STATE.ACTIVE
+cell_states[0, 2] = CELL_STATE.PREDICTING
+
+new_cell_states = np.zeros((COLUMN_COUNT, COLUMN_HEIGHT), dtype=np.int32)
 ##SAMPLE INPUT####################
+
+
 
 
 active_connections = np.equal(cell_states[temporal_connections[:,
@@ -34,14 +39,14 @@ active_connections = np.equal(cell_states[temporal_connections[:,
 predicting_connections = np.equal(cell_states[temporal_connections[:,
                                                                    :, :, 0], temporal_connections[:, :, :, 1]], CELL_STATE.PREDICTING)
 
+permenance_values_mask = permenance_values >= PERMENANCE_THRESHOLD
 
 connected_active_conns = np.logical_and(
-    active_connections, permenance_values >= PERMENANCE_THRESHOLD
-)
+    active_connections, permenance_values_mask)
 
 connected_predic_conns = np.logical_and(
-    predicting_connections, permenance_values >= PERMENANCE_THRESHOLD
-)
+    predicting_connections, permenance_values_mask)
+
 
 connected_active_conns_overlap = np.sum(connected_active_conns, axis=2)
 connected_predic_conns_overlap = np.sum(connected_predic_conns, axis=2)
